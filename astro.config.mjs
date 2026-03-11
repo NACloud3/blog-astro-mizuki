7d1f67a034e4e13303868129e3a39d351b903637
unknown <1520273962@qq.com>
2026-03-11 10:55:11 +0800

    chore: configure GitHub Pages and Decap CMS

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,7 +32,7 @@ import { remarkFixGithubAdmonitions } from "./src/plugins/remark-fix-github-admo
 // https://astro.build/config
 export default defineConfig({
 	site: siteConfig.siteURL,
-	base: "/",
+	base: "/blog-astro-mizuki/",
 	trailingSlash: "always",
 
 	output: "static",
